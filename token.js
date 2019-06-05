9c59849720c823db7bc876691f98d5818e81a328
agostrendsetter <51096094+agostrendsetter@users.noreply.github.com>
2019-06-05 21:50:43 +0200

    Add files via upload

---
+++ token.js
@@ -0,0 +1,162 @@
+$(document).ready(function () {
+    var addr = getParam('a');
+    var contractAddr = getParam('c');
+
+    checkNetwork();
+
+    if (!$.isEmptyObject(addr) && !$.isEmptyObject(contractAddr)) {
+        $('#searchAddr').val(addr);
+        $('#searchContractAddr').val(contractAddr);
+        getToken();
+    }
+
+});
+
+$(document).on('click','.btn-go',function(){
+    getToken();       
+  });
+
+
+function getToken() {
+    $('.data-info').empty(); 
+    positionFooter();
+
+    var addr = $('#searchAddr').val();
+    var contractAddr = $('#searchContractAddr').val();
+    var selectedNetwork = getSelectedMainnet();
+    var selectedTestNetwork = getSelectedTestnet();
+
+    if (selectedNetwork.length === 0 && selectedTestNetwork === 0){
+        alert('Please select network');
+        return;
+    } else if ((addr.length === 0) || (contractAddr.length === 0)){
+        return;
+    } else if (validateHash(40, addr) === false){
+
+        if (addr.length === 40 ){
+            addr = '0x' + addr;
+            if (validateHash(40, '0x' + addr) == false) {
+                alert('Invalid Address');
+                return;
+            }
+        } else {
+            alert('Invalid Address');
+            return;
+        }     
+    }  else if (validateHash(40, contractAddr) === false){
+
+        if (contractAddr.length === 40 ){
+            contractAddr = '0x' + contractAddr;
+            if (validateHash(40, '0x' + contractAddr) == false) {
+                alert('Invalid Contract Address');
+                return;
+            }
+        } else {
+            alert('Invalid Contract Address');
+            return;
+        }     
+    }
+
+    $('.loader').show();
+    var count = 0;
+    var totalSelectedNetwork = selectedNetwork.length + selectedTestNetwork.length;
+  
+    $.each(selectedNetwork, function (key, value) {     
+        callMainnetNetwork('',addr, contractAddr, value, 3)
+            .then(function (data) {
+                if (data.error) {
+                    generateTxErr(data.error, value);
+                } else {
+                    getUSDPrice(data.symbol, 'USD')
+                        .then(function (usdPrice) {                          
+                            generateTokenInfo(data, value, usdPrice, contractAddr);
+                        });                   
+                }
+
+                count++;
+
+                if (count == totalSelectedNetwork) {
+                    $('.loader').hide();
+                    $('.datasource').show();
+                    positionFooter();
+                }
+
+            }, function(err){
+                generateTxErr(err.statusText, value);
+
+                count++;
+                
+                if (count == totalSelectedNetwork) {
+                    $('.loader').hide();
+                    $('.datasource').show();
+                    positionFooter();
+                }
+            });
+    });
+
+    $('.loader').show();
+
+    $.each(selectedTestNetwork, function (key, value) {     
+        callTestnetNetwork('',addr, contractAddr, value,3)
+            .then(function (data) {
+                if (data.error) {
+                    generateTxErr(data.error, value);
+                } else {
+                    getUSDPrice(data.symbol, 'USD')
+                        .then(function (usdPrice) {                          
+                            generateTokenInfo(data, value, usdPrice, contractAddr);
+                        });
+                }
+
+                count++;
+
+                if (count == totalSelectedNetwork) {
+                    $('.loader').hide();
+                    $('.datasource').show();
+                    positionFooter();
+                }
+
+            }, function(err){
+                generateTxErr(err.statusText, value);
+
+                count++;
+                
+                if (count == totalSelectedNetwork) {
+                    $('.loader').hide();
+                    $('.datasource').show();
+                    positionFooter();
+                }
+            });
+    });
+
+
+}
+
+
+function generateTokenInfo(data, network, usdPrice, contractAddr) {
+
+    try {
+        var header = '<div class="card mt-3"> <div class="card-body"> <h5 class="card-title">{{network}}</h5>';
+        var output = '';
+
+        if (data.result == null) {
+            eader += '<div class="row"><div class="col-sm-12">Token Balance not found</div></div>';
+            output = header.replace('{{network}}', network);
+        } else {
+            var lbl = '<div class="row mb-1"><div class="col-sm-2">{{label}}:</div><div class="col-sm-9">{{value}}</div></div>';
+            output = header.replace('{{network}}', network);
+
+            var balance = convertDecimals(parseInt(data.decimal), new BigNumber(data.result));
+            var value = usdPrice.USD ? new BigNumber(balance * usdPrice.USD) + ' USD  <font size="1">(@' + usdPrice.USD + '/USD)</font>' : '0.00000000';
+            output += lbl.replace('{{label}}', 'Balance').replace('{{value}}', balance + ' <a href="https://etherscan.io/token/' + contractAddr + '" rel="nofollow">' + data.symbol + '</a>');
+            output += lbl.replace("{{label}}", 'Value').replace('{{value}}', value);
+            output += lbl.replace("{{label}}", 'Decimal').replace('{{value}}', data.decimal);
+
+        }
+
+        $('.data-info').append(output);
+    } catch (err) {
+        generateTxErr(err, network);
+    }
+
+}
